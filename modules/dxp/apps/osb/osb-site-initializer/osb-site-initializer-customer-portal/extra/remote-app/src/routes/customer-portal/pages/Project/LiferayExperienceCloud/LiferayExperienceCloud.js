12ee13e390c5c9e3a7110bc08a1aeebe86df91fe
Wanderson Vicente <wanderson.vicente@liferay.com>
2022-07-21 18:00:42 -0300

    LPS-158363 Create LXC component and index

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Project/LiferayExperienceCloud/LiferayExperienceCloud.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+// import SetupLiferayExperienceCloud from '../../../components/ActivationStatus/LiferayExperienceCloud/LiferayExperienceCloud';
+
+const LiferayExperienceCloud = () => {
+	return (
+		<div className="mr-4">
+			<h1>Estou em Product/LiferayExperienceCloud</h1>
+		</div>
+	);
+};
+
+export default LiferayExperienceCloud;
